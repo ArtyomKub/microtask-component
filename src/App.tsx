bdbf5e8c52d06e3a8b87239602edc576e6ecea2e
ArtyomKub <coder.kub.lover@gmail.com>
2023-05-26 22:27:56 +0200

    Add props in Footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Header} from "./site/Header";
 import {Footer} from "./site/Footer";
+import Body from "./site/Body";
 
 function App() {
     return (
         <div>
-            <Header title={'NEW BODY'}/>
-            <Footer/>
+            <Header title={'NEW HEADER'}/>
+            <Body titleForBody={'NEW BODY'}/>
+            <Footer titleForFooter={'NEW FOOTER'}/>
         </div>
     );
 }
